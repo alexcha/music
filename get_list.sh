5f91948f3c7895490ad2657e8e94669258bb5e2d
alexcha <netggio@gmail.com>
2023-10-15 00:39:44 +0900

    Update get_list.sh

--- get_list.sh
+++ get_list.sh
@@ -21,7 +21,7 @@ else
        fi
 done
 
-id_one=(`cat list2.txt| grep 'File1=http' |awk -F 'File1=' '{for (i=2; i<=NF; i++) print $i}'` )
+id_one=(`cat list2.txt| grep File1\=http |awk -F 'File1=' '{for (i=2; i<=NF; i++) print $i}'` )
 name_one=(`cat list2.txt| grep Title1| awk -F 'Title1=' '{for (i=2; i<=NF; i++) print $i}'| sed  's/ /_/g'`)
 
 id=(${id_one[@]} )
