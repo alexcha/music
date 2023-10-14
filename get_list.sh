d8af8781a5ccef3e8390bcc702b29c0fc14d89fd
alexcha <netggio@gmail.com>
2023-10-14 21:57:41 +0900

    Update get_list.sh

--- get_list.sh
+++ get_list.sh
@@ -3,26 +3,34 @@ echo "#"
 echo "# 엄마/아빠를 위한~"
 echo \# LastUpdate: `date -d +9hour "+%Y-%m-%d %H:%M"`
 
+export LANG="ko_KR.UTF-8"
+echo > list2.txt
+for i in `cat list.txt`
+do
+        ck=`curl -s $i.inlive.co.kr/live/listen.pls | grep  File1\=http`
+
+       if [ $? -eq 0 ]
+
+       then
+
+       curl -s $i.inlive.co.kr/live/listen.pls | grep -A 1 File1\=http  >> list2.txt
+else
+       echo ''
+       fi
+done
+
+id_one=(`cat list2.txt| grep File1\=http `)
+name_one=(`cat list2.txt| grep Title1`)
+
+id=(${id_one[@]} )
+name=(${name_one[@]} )
+
 echo [playlist]
-
-one=`curl -s "http://saycast.sayclub.com/saycast/index/station?kind=genrelist&catGenre=mTR&page=1" -o  1.txt`
-sleep 10
-two=`curl -s "http://saycast.sayclub.com/saycast/index/station?kind=genrelist&catGenre=mTR&page=2" -o  2.txt`
-
-id_one=(`cat 1.txt | grep ic_onairS.gif  | awk '{print $2}'  | awk -F "/" '{print $7 }' | sed 's/[<>,]/ /g' | tr -d '"' | awk '{print $1}'`)
-name_one=(`cat 1.txt | grep ic_onairS.gif  | awk '{print $2}'  | awk -F "/" '{print $7 }' | sed 's/[<>,]/ /g' | tr -d '"' | awk '{print $2}'`)
-
-name_two=(`cat 2.txt | grep ic_onairS.gif  | awk '{print $2}'  | awk -F "/" '{print $7 }' | sed 's/[<>,]/ /g' | tr -d '"' | awk '{print $2}'`)
-id_two=(`cat 2.txt | grep ic_onairS.gif  | awk '{print $2}'  | awk -F "/" '{print $7 }' | sed 's/[<>,]/ /g' | tr -d '"' | awk '{print $1}'`)
-
-id=(${id_one[@]} ${id_two[@]} )
-name=(${name_one[@]} ${name_two[@]} )
-
 length=${#id[@]}
 for j in `seq 0 $(($length-1))`
 do
-echo "File$(($j+1))=http://${id[$j]}.saycast.com"
-echo "Title$(($j+1))=${name[$j]}"
+        echo "File$(($j+1))=http://${id[$j]}.saycast.com"
+        echo "Title$(($j+1))=${name[$j]}"
 done
 
 
